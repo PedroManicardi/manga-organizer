9638aa98fe315d8a3f1d324a6e18908d8693d490
carlosnrib <carlosnribeiro3@gmail.com>
2024-06-15 16:03:26 -0300

    Voltando pra c

--- src/manga.h
+++ src/manga.h
@@ -1,40 +1,27 @@
 #ifndef MANGA_H
 #define MANGA_H
 
-#include <string>
-#include <vector>
-
+#define MAX 100
 #define FILE_NAME "mangas.txt"
-#define PRIMARY_INDEX_FILE "primary_index.txt"
-#define SECONDARY_INDEX_FILE "secondary_index.txt"
 
-class Manga {
-public:
-    std::string ISBN;
-    std::string title;
-    std::string authors;
-    std::string genre;
-    std::string magazine;
-    std::string publisher;
+typedef struct {
+    char ISBN[20];
+    char title[MAX];
+    char authors[MAX];
+    char genre[MAX];
+    char magazine[MAX];
+    char publisher[MAX];
     int start_year;
     int end_year;
     int edition_year;
     int total_volumes;
     int acquired_volumes;
-    std::vector<int> acquired_volumes_list;
+    int acquired_volumes_list[MAX];
+} Manga;
 
-    Manga();
-    void input();
-    void print() const;
-};
-
-void addManga(std::vector<Manga>& mangas);
-void listMangas(const std::vector<Manga>& mangas);
-void saveMangasToFile(const std::vector<Manga>& mangas);
-std::vector<Manga> loadMangasFromFile();
-void updateManga(std::vector<Manga>& mangas);
-void deleteManga(std::vector<Manga>& mangas);
-void updatePrimaryIndex(const std::vector<Manga>& mangas);
-void updateSecondaryIndex(const std::vector<Manga>& mangas);
+void addManga(Manga mangas[], int *count);
+void listMangas(Manga mangas[], int count);
+void saveMangasToFile(Manga mangas[], int count);
+int loadMangasFromFile(Manga mangas[]);
 
 #endif
